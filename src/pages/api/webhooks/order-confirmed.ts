c5d54e0ed9496bf69ab51790cb23ee86667e488d
fedexin40 <federico.cruz.lozano@hotmail.com>
2024-07-22 21:38:04 -0600

    Fix the fulfill creation

--- src/pages/api/webhooks/order-confirmed.ts
+++ src/pages/api/webhooks/order-confirmed.ts
@@ -1,13 +1,14 @@
 import { SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
-import { saleorApp } from "../../../saleor-app";
+import { NextApiHandler } from "next";
 import { gql } from "urql";
 import {
-  OrderConfirmWebhookPayloadFragment,
-  UpdateOrderTrackingNumberDocument,
+  FulFillOrderDocument,
+  OrderConfirmWebhookPayloadFragment
 } from "../../../../generated/graphql";
 import { api } from "../../../lib/axios";
-import { NextApiHandler } from "next";
 import { createClient } from "../../../lib/create-graphq-client";
+import { saleorApp } from "../../../saleor-app";
+
 
 const OrderConfirmWebhookPayload = gql`
   fragment OrderConfirmWebhookPayload on OrderConfirmed {
@@ -54,6 +55,7 @@ const OrderConfirmWebhookPayload = gql`
       }
       channel {
         warehouses {
+          id
           address {
             postalCode
             streetAddress1
@@ -110,6 +112,21 @@ const OrderUpdateTrackingNumber = gql`
   }
 `;
 
+const FulfillOrder = gql`
+  mutation fulFillOrder($orderId: ID!, $input: OrderFulfillInput!) {
+    orderFulfill(order: $orderId, input: $input) {
+      errors {
+        code
+        message
+        field
+      }
+      fulfillments {
+        id
+      }
+    }
+  }
+`;
+
 export const orderConfirmedWebhook = new SaleorAsyncWebhook<OrderConfirmWebhookPayloadFragment>({
   name: "Order Confirmed",
   webhookPath: "api/webhooks/order-confirmed",
@@ -128,7 +145,6 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
 
   return orderConfirmedWebhook.createHandler(async (req, res, ctx) => {
     console.log("webhook received");
-    console.log(req.body);
 
     const { payload, authData, event } = ctx;
 
@@ -138,8 +154,6 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
      */
 
     const order = payload.order;
-
-    console.log({ order });
 
     if (!order || !order.deliveryMethod)
       return res.status(200).json({ message: "missing delivery method" });
@@ -161,9 +175,9 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
         parcels: [
           {
             weight: 1,
-            height: 1,
-            width: 1,
-            length: 1,
+            height: 10,
+            width: 10,
+            length: 10,
             distance_unit: "CM",
             mass_unit: "KG",
           },
@@ -171,11 +185,13 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
         address_to: {
           name: order.userEmail,
           address1: shipping_address.streetAddress1,
+          address2: shipping_address.streetAddress2,
           city: shipping_address.city,
           province: shipping_address.countryArea || shipping_address.city,
           country: "MX",
           zip: shipping_address.postalCode,
           reference: "Frente a tienda de abarro",
+          phone: shipping_address.phone,
           contents: order.lines
             .map((line) => `${line.quantity} x ${line.productName} - ${line.variantName}`)
             .join("\n"),
@@ -183,24 +199,23 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
         address_from: {
           name: "warehouse",
           address1: warehouse_addess.streetAddress1,
+          address2: warehouse_addess.streetAddress2,
           city: warehouse_addess.city,
           province: warehouse_addess.countryArea,
           country: warehouse_addess.country.code,
           zip: warehouse_addess.postalCode,
+          phone: warehouse_addess.phone
         },
         consignment_note_class_code: "14111500",
         consignment_note_packaging_code: "1H1",
       };
 
-      console.log(JSON.stringify(body, null, 2));
       const { data: shipment } = await api.post("/v1/shipments", body).catch((err) => {
         console.log(err.response.data);
         return { data: undefined };
       });
 
       if (!shipment) return res.status(200).json({ message: "shipment creation failed" });
-
-      // console.log(shipment.included);
 
       if (!Object.keys(order.deliveryMethod ?? {}).includes("id"))
         return res.status(200).json({ message: "missing delivery method" });
@@ -216,8 +231,6 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
           return atob(delivered_id) === saleor_id;
         });
 
-      console.log(rate);
-
       if (!rate) return res.status(200).json({ message: "rate not found" });
 
       const { data: label } = await api
@@ -232,34 +245,30 @@ const orderConfirmedHandler: NextApiHandler = async (req, res) => {
 
       if (!label) return res.status(200).json({ message: "label creation failed" });
 
-      console.log(label);
-
       const tracking_number = label.data.attributes.tracking_number;
 
-      // save tracking number to order
-      const { data: updated_order } = await api
-        .patch(`/v1/orders/${order.id}`, {
-          tracking_number,
-        })
-        .catch((err) => {
-          console.log(err.response.data);
-          return { data: undefined };
-        });
-
-      const fulfillment = await client.query(UpdateOrderTrackingNumberDocument, {
+      const lines = order.lines.map((line) => {
+        return {
+          stocks: [{quantity: line.quantity, warehouse: order.channel.warehouses[0].id}],
+          orderLineId: line.id 
+        }
+      })
+      const fulfillment = await client.mutation(FulFillOrderDocument, {
         orderId: order.id,
-        trackingNumber: tracking_number,
+        input: {
+          lines: lines,
+          notifyCustomer: true,
+          trackingNumber: tracking_number
+        },
       });
 
-      if (fulfillment.data?.updateMetadata?.errors) {
-        console.log(fulfillment.data.updateMetadata.errors);
-        return res.status(200).json({ message: fulfillment.data.updateMetadata.errors[0].message });
+      if (fulfillment.data?.orderFulfill?.errors && fulfillment.data.orderFulfill.errors.length > 0 ) {
+        console.log(fulfillment.data?.orderFulfill?.errors);
+        return res.status(200).json({ message: fulfillment.data.orderFulfill.errors[0].message });
       }
 
-      if (fulfillment.data?.updateMetadata?.item) {
-        console.log(fulfillment.data.updateMetadata.item);
-        return res.status(200).json({ message: "tracking number saved" });
-      }
+      return res.status(200).json({ message: "tracking number saved" });
+      
     } catch (err) {
       console.log({ err });
     }
